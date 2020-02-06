fc16ea9bca073c1a824d35316930658638b8f71f
JleenDeniel <79879013038@yandex.ru>
2020-02-06 20:38:26 +0300

    бдшка работает!!!

--- app/__init__.py
+++ app/__init__.py
@@ -1,116 +1,86 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+from flask import Flask, request, render_template, url_for, make_response, jsonify
+# from app.db import ConnectionToDB
+# import pymysql
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from flask import redirect, url_for, render_template, flash
-from flask_login import current_user, login_user, logout_user
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
 app.config.update(dict(
-        SECRET_KEY="powerful secretkey",
-        WTF_CSRF_SECRET_KEY="a csrf secret key"
-    ))
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1T0bms93g5gK'
-app.config['MYSQL_DB'] = 'election'
-
+    SECRET_KEY="powerful secret_key",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1T0bms93g5gK@localhost/election'
 
-db = SQLAlchemy(app)
-login = LoginManager(app)
-login.login_view = 'login'
 
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign in')
-
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Use a different name')
-
-    def validate_email(self, email):
-        email = User.query.filter_by(email=email.data).first()
-        if email is not None:
-            raise ValidationError('This email has already registered')
-
-
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id_user = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(password)
-
-    def load_user(id_user):
-        return User.query.get(int(id_user))
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    res = {'action': 'login'}
+    if request.method == 'POST':
+        username = request.form.get('userName')
+        password = request.form.get('userPassword')
+        # result = get_user(username, password)
+        user = User.query.filter_by(email=username).first()
+        res['status'] = user.login
+        return jsonify(res)
+    else:
+        res['status'] = 'get'
+        # return render_template('index.html')
+        return jsonify(res)
 
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('base'))
-
-@app.route('/')
-def start():
-    return render_template('base.html')
-
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-
+# todo обработка формы регистрации
 @app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm();
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('You are registered user now!')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+def registration():
+    if request.method == 'POST':
+        login = request.form.get('userName')
+        email = request.form.get('userEmail')
+        password = generate_password_hash(request.form.get('userPassword'))
+        print(request.form)
+        res = reg_user(login, password, email)
+        return make_response(jsonify(result=res))
+    else:
+        return render_template('index.html')
+
+
+# connection = pymysql.connect('localhost', 'root', '1T0bms93g5gK', 'election')
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id_user = db.Column(db.Integer(), primary_key=True)
+    login = db.Column(db.String(30))
+    password = db.Column(db.String(255))
+    email = db.Column(db.String(30))
+
+#
+# def get_user(username, password):
+#     login = ""
+#     with connection:
+#         cur = connection.cursor()
+#         cur.execute("Select login from users where email = %s and password = %s;", (username, password))
+#         print(cur.fetchone())
+#
+#         return login
+#
+#
+# def reg_user(username, password, email):
+#     with connection:
+#         cursor = connection.cursor()
+#         sql = "Insert into users(login, password, email) values(%s, %s, %s);"
+#         query = cursor.execute(sql, (username, password, email))
+#         connection.commit()
+#
+#         if query > 0:
+#             return "registered"
+#         else:
+#             return "not registered"
+
+
+if __name__ == "__main__":
+    app.run(Debug=True)
