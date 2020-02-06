d98a9d221e5231e65a0b83c16f9b892c7347832c
JleenDeniel <79879013038@yandex.ru>
2020-02-06 23:58:17 +0300

    backup...

--- app/static/src/login.js
+++ app/static/src/login.js
@@ -79,17 +79,9 @@ function do_able() {
 	document.getElementById('btn_search').disabled = false;
 }
 
-//отправка формы на сервер
-let login_form = document.querySelector("form");
-login_form.addEventListener('submit', form_send);
-function form_send(){
-	// let form_data = {
-	// 	"name": login_form.elements['email_input'].value,
-	// 	"password": login_form.elements['password_input'].value
-	// };
-	// let json_data = JSON.stringify(form_data);
-}
-
+//отправка get запроса за данными с сервера
+//существует ли такой пользователь в базе или нет
+let user_in_base_flag = false; //флаг отвечающий за проверку есть ли пользователь в базе
 var xhr = new XMLHttpRequest();
 xhr.open(
 	'GET',
@@ -98,11 +90,23 @@ xhr.open(
 );
 xhr.send();
 
-xhr.onreadystatechangeы = function(){
+xhr.onreadystatechange = function(){
 	if (xhr.readyState !== 4) return;
 	if (xhr.status === 200){
-		console.log('result', JSON.parse(xhr.responseText).bool);
+		console.log('result: ', JSON.parse(xhr.responseText));
+
+		if (JSON.parse(xhr.responseText).bool){
+			$("#enter_link").html(JSON.parse(xhr.responseText).username);
+			user_in_base_flag = true;
+		}
+
 	} else {
+		user_in_base_flag = false;
 		console.log('err', xhr.responseText);
 	}
 }
+
+//обработка клика на кнпку submit на форме
+$("#data_form").submit(function(){
+	alert(user_in_base_flag);
+});
