4a66da340886a8a6b341c505cc6653a1020b5740
JleenDeniel <79879013038@yandex.ru>
2020-02-14 17:38:26 +0300

    нуууууууу оно работает

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, render_template, make_response, jsonify
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, current_user
+
+app = Flask(__name__)
+CORS(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+app.config.update(dict(
+    SECRET_KEY="powerful secret_key",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1T0bms93g5gK@localhost/election'
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+bool = False
+res_login = {'bool': bool, 'username': ''}
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('userName')
+        password = request.form.get('userPassword')
+
+        if '@' in username:
+            user = User.query.filter_by(email=username).first()
+            if check_password_hash(user.password, password):
+                bool = True
+                res_login['bool'] = bool
+                res_login['username'] = user.login
+            else:
+                bool = False
+                res_login['bool'] = bool
+        else:
+            user = User.query.filter_by(login=username).first()
+            if check_password_hash(user.password, password):
+                bool = True
+                res_login['bool'] = bool
+                res_login['username'] = user.login
+            else:
+                bool = False
+                res_login['bool'] = bool
+        print(user)
+        return render_template('index.html')
+    else:
+        return jsonify(res_login)
+
+
+# обработка формы регистрации, если введенный логин или почта уже существуют в бд, то в поле status
+# будет написано что именно
+res_register = {'bool': False, 'username': '', 'status': ''}
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def registration():
+    if request.method == 'POST':
+        login_reg = request.form.get('userNameReg')
+        email = request.form.get('userEmailReg')
+        password = generate_password_hash(request.form.get('userPasswordReg'))
+        if User.query.filter_by(login=login_reg).first():
+            res_register['bool'] = False
+            res_register['status'] = 'username_exists'
+        elif User.query.filter_by(email=email).first():
+            res_register['bool'] = False
+            res_register['status'] = 'email_exists'
+        else:
+            user = User(login_reg, email, password)
+            res_register['bool'] = user.create_user()
+            res_register['username'] = login_reg
+
+        return render_template('index.html')
+    else:
+        return jsonify(res_register)
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id_user = db.Column(db.Integer(), primary_key=True)
+    login = db.Column(db.String(30))
+    password = db.Column(db.String(255))
+    email = db.Column(db.String(30))
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/src/login.js
@@ -0,0 +1,121 @@
+//поле входа
+let field_login = document.getElementById('field_login');
+
+//поле регистрации
+let registration_field = document.getElementById('registration_field');
+
+//показать окно входа при нажатии в меню на сылку "Войти"
+document.getElementById('enter_link').addEventListener('click', show_login_field);
+function show_login_field(){
+	$('#email_input').val(''); //чистит поле с username
+	$('#password_input').val(''); //чистим поле с паролем
+	do_disable();
+	field_login.style.display = 'block';
+}
+
+//показать окно регистрации при нажатии на ссылку "регистрация"
+//в поле входа 
+let flag_reg_open = false; //отслеживание нажата ли ссылку "регистрация" 
+document.getElementById('registration').addEventListener('click', show_reg_field);
+function show_reg_field(){
+	flag_reg_open = true;
+	do_disable();
+	registration_field.style.display = 'block';
+	close_login_btn();
+}
+
+//закрытие окна входа при нажатии вне области окна
+field_login.addEventListener('click', close_login);
+function close_login(e){
+	if ($(e.target).is("#field_login")){
+		field_login.style.display = 'none';
+		do_able();
+	}
+}
+
+//закрытие окна регистрации при клике вне области окна
+registration_field.addEventListener('click', close_reg);
+function close_reg(e){
+	if ($(e.target).is("#registration_field")){
+		registration_field.style.display = 'none';
+		flag_reg_open = false;
+		do_able();
+	}
+}
+
+//закрытие окна входа при нажатии на кнопку закрытия
+document.getElementById('close_btn').addEventListener('click', close_login_btn);
+function close_login_btn(){
+	if (!flag_reg_open) do_able();
+	field_login.style.display = 'none';
+}
+
+//закрытие окна регистрации при нажатии на кнопку закрытия
+document.getElementById('close_btn_reg').addEventListener('click', close_reg_btn);
+function close_reg_btn(){
+	flag_reg_open = false;
+	do_able();
+	registration_field.style.display = 'none';
+}
+
+//массив ссылок меню
+let nav_link_arr = document.getElementsByClassName('nav-link');
+
+//делает ссылки неактиными (nav-link)
+//делвет поле поиска неактивным вместе с кнопкой
+function do_disable(){
+	for (let i = 0; i < nav_link_arr.length; i++){
+		nav_link_arr[i].classList.add('disabled');
+	}
+	document.getElementById('search_input').disabled = true;
+	document.getElementById('btn_search').disabled = true;
+	$('#menu').css('opacity', '0.3');
+}
+
+//делает ссылки активными (nav-link)
+//делает поле поиска активным вместе с кнопкой
+function do_able() {
+	for (let i = 0; i < nav_link_arr.length; i++){
+		nav_link_arr[i].classList.remove('disabled');
+	} 
+	document.getElementById('search_input').disabled = false;
+	document.getElementById('btn_search').disabled = false;
+	$('#menu').css('opacity', '0.9');
+}
+
+//отправка get запроса за данными login с сервера
+//существует ли такой пользователь в базе или нет
+$.get(
+  '/login',
+  function(data) {             
+   	console.log(data.bool, data.username); //вывод в консоль
+   	let login_value = data.username;
+   	if (data.bool == true){
+			$("#enter_link").html(data.username);
+			$("#close_btn_logined").css('display', 'block');
+		}
+		//прячем кнопку выхода из профиля, чистим данные
+		$('#close_btn_logined').click(function(){
+			$("#enter_link").html('Войти');
+			$('#close_btn_logined').css('display', 'none');;
+			data.bool = false;
+			data.username = '';
+			console.log(data);
+		});
+  }
+);
+
+//проверять в поле регистрации совпадет ли пароль и его подтверждение
+
+//отправка get запроса за данными Register c сервера
+//существует ли такой пользователь в базе или нет
+//на нажатие кнопки зарегистрироваться
+$('#register_data').submit(function(){
+	$.get(
+  	'http://localhost:5000/register',
+  	function(data){              
+   		console.log(data); //вывод в консоль
+  	}
+	);
+});
+ 
